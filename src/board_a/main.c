c858c750c0b870334a47d78e676d9bc1e9577edd
Anthony <acgardner95@gmail.com>
2025-12-19 16:26:15 +0400

    debug

--- src/board_a/main.c
+++ src/board_a/main.c
@@ -1,21 +1,15 @@
-#include "can.h"
-#include "clock.h"
-#include "gpio.h"
 #include "stm32f767xx.h"
 
 int main(void) {
-    clock_init();
-    led_init();
-    can_init();
-
-    uint8_t counter = 0;
+    // Enable GPIOB clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+    
+    // Set PB0 as output
+    GPIOB->MODER &= ~GPIO_MODER_MODER0;
+    GPIOB->MODER |= (0x1 << GPIO_MODER_MODER0_Pos);
     
     while (1) {
-        uint8_t data[8] = {counter++, 0, 0, 0, 0, 0, 0, 0};
-        can_transmit(0x100, data, 1);
-        
-        led_toggle();
-        
+        GPIOB->ODR ^= GPIO_ODR_OD0;
         for (volatile int i = 0; i < 500000; i++);
     }
 }
