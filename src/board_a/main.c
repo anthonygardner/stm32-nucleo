e3d22826011fe25b680bb5cb80036b8b8b946822
Anthony <acgardner95@gmail.com>
2025-12-19 16:27:14 +0400

    debug

--- src/board_a/main.c
+++ src/board_a/main.c
@@ -1,15 +1,13 @@
 #include "stm32f767xx.h"
+#include "clock.h"
+#include "gpio.h"
 
 int main(void) {
-    // Enable GPIOB clock
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-    
-    // Set PB0 as output
-    GPIOB->MODER &= ~GPIO_MODER_MODER0;
-    GPIOB->MODER |= (0x1 << GPIO_MODER_MODER0_Pos);
+    clock_init();
+    led_init();
     
     while (1) {
-        GPIOB->ODR ^= GPIO_ODR_OD0;
+        led_toggle();
         for (volatile int i = 0; i < 500000; i++);
     }
 }
