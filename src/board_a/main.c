b68810e86d5defece1ab5661e837f03ad226b1ab
Anthony <acgardner95@gmail.com>
2025-12-26 20:58:08 +0400

    more debugging

--- src/board_a/main.c
+++ src/board_a/main.c
@@ -6,31 +6,24 @@
 #include "stm32f767xx.h"
 
 int main(void) {
-    // "I'm alive" sanity check
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
     GPIOB->MODER |= (1 << 0);
-    GPIOB->ODR |= (1 << 0);
-
+    
     clock_init();
-    GPIOB->ODR ^= (1 << 0);
-
     led_init();
     uart_init();
-    GPIOB->ODR ^= (1 << 0);
-
     can_init();
-    GPIOB->ODR ^= (1 << 0);
-
     eth_init();
-    GPIOB->ODR ^= (1 << 0);
     
     uint8_t counter = 0;
     
     while (1) {
-        uint8_t data[8] = {counter++, 0, 0, 0, 0, 0, 0, 0};
-        can_transmit(0x100, data, 1);
+        // uint8_t data[8] = {counter++, 0, 0, 0, 0, 0, 0, 0};
+        // can_transmit(0x100, data, 1);
         
-        led_toggle();
+        // led_toggle();
+
+        GPIOB->ODR ^= (1 << 0);
         for (volatile int i = 0; i < 500000; i++);
     }
 }
