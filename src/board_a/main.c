7c6b5e14afeae1d36f2ddf16d5e189fceb98f257
Anthony <acgardner95@gmail.com>
2025-12-27 21:15:19 +0400

    sigh

--- src/board_a/main.c
+++ src/board_a/main.c
@@ -1,28 +1,28 @@
 #include "stm32f767xx.h"
 
 int main(void) {
-    // Enable GPIOC clock
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+    // Enable GPIOD clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
     
-    // Enable USART6 clock
-    RCC->APB2ENR |= (1 << 5);
+    // Enable USART3 clock
+    RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
     
-    // PC6 as alternate function
-    GPIOC->MODER &= ~(0b11 << 12);
-    GPIOC->MODER |= (0b10 << 12);
-    GPIOC->AFR[0] &= ~(0xF << 24);
-    GPIOC->AFR[0] |= (8 << 24);
+    // PD8 as alternate function
+    GPIOD->MODER &= ~(0b11 << 16);
+    GPIOD->MODER |= (0b10 << 16);
+    GPIOD->AFR[1] &= ~(0xF << 0);
+    GPIOD->AFR[1] |= (7 << 0);  // AF7 for USART3
     
-    // Baud rate for 32 MHz HSI
-    USART6->BRR = 285;
+    // Baud rate for 16 MHz HSI
+    USART3->BRR = 139;
     
     // Enable TX and USART
-    USART6->CR1 = (1 << 0) | (1 << 3);
+    USART3->CR1 = (1 << 0) | (1 << 3);
     
     // Send 'U' forever
     while (1) {
-        while (!(USART6->ISR & (1 << 7)));
-        USART6->TDR = 'U';
+        while (!(USART3->ISR & (1 << 7)));
+        USART3->TDR = 'U';
         for (volatile int i = 0; i < 500000; i++);
     }
 }
