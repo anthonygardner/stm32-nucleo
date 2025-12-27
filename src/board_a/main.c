83bd56d885ccb299157438a1bc5cd3ff8815dbe2
Anthony <acgardner95@gmail.com>
2025-12-27 20:49:06 +0400

    debug

--- src/board_a/main.c
+++ src/board_a/main.c
@@ -1,36 +1,28 @@
-#include "can.h"
-#include "clock.h"
-#include "eth.h"
-#include "gpio.h"
-#include "uart.h"
 #include "stm32f767xx.h"
 
 int main(void) {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-    GPIOB->MODER |= (1 << 14);
-    GPIOB->ODR |= (1 << 7); // Blue LED on
-
-    clock_init();
-    led_init();
-    uart_init();
-
-    uart_send_char('H');
-    uart_send_char('i');
-    uart_send_char('\r');
-    uart_send_char('\n');
-
-    can_init();
-    eth_init();
+    // Enable GPIOC clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
     
-    uint8_t counter = 0;
+    // Enable USART6 clock
+    RCC->APB2ENR |= (1 << 5);
     
+    // PC6 as alternate function
+    GPIOC->MODER &= ~(0b11 << 12);
+    GPIOC->MODER |= (0b10 << 12);
+    GPIOC->AFR[0] &= ~(0xF << 24);
+    GPIOC->AFR[0] |= (8 << 24);
+    
+    // Baud rate for 16 MHz HSI
+    USART6->BRR = 139;
+    
+    // Enable TX and USART
+    USART6->CR1 = (1 << 3) | (1 << 13);
+    
+    // Send 'U' forever
     while (1) {
-        uint8_t data[8] = {counter++, 0, 0, 0, 0, 0, 0, 0};
-        
-        can_transmit(0x100, data, 1);
-        
-        led_toggle();
-
+        while (!(USART6->ISR & (1 << 7)));
+        USART6->TDR = 'U';
         for (volatile int i = 0; i < 500000; i++);
     }
 }
