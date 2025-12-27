c7a9f2164372e304364175e349f581d9f11851c5
Anthony <acgardner95@gmail.com>
2025-12-27 18:49:30 +0400

    adjust baud rate

--- src/common/uart.c
+++ src/common/uart.c
@@ -36,7 +36,7 @@ void uart_init(void) {
     uart_configure_pins();
 
     // Configure baud rate
-    USART6->BRR = 10000;
+    USART6->BRR = 833;
 
     // Enable transmitter and USART6
     USART6->CR1 |= (1 << 3) | (1 << 13);
